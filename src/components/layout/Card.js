97eeb659735d522f8166ae7655efed182ba466ab
kauly-dot-repo <63747542+kauly-dot-repo@users.noreply.github.com>
2021-10-19 17:09:03 +0200

    Delete src directory

--- src/components/layout/Card.js
+++
@@ -1,9 +0,0 @@
-import classes from './Card.module.css'
-
-function Card(props) {
-  return <div className={classes.card}>
-    {props.children} {/* //children is the content between open and closing tag */}
-  </div>
-}
-
-export default Card;
--- src/pages/user/User.js
+++
@@ -1,6 +0,0 @@
-
-export default function User() {
-  return (
-    <h1>Welcome Guest User</h1>
-  );
-}
